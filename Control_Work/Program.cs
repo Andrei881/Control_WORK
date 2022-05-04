5e06b681997c7607d31ba4b07d92c1a0a48b6009
Andrey <dron881@mail.ru>
2022-05-04 18:40:08 +0300

    string2

--- Control_Work/Program.cs
+++ Control_Work/Program.cs
@@ -1,2 +1,14 @@
-﻿string[] ar1 = new string[] {"730", "jump","game", "92","ant"};
-string[] ar2 = new string[ar1.Length];
+﻿string[] ar = new string[] {"730", "jump","game", "92","ant"};
+string[] ar2 = new string[ar.Length];
+void SAr(string[] ar, string[] ar2)
+{
+    int c = 0;
+    for (int i = 0; i < ar.Length; i++)
+    {
+    if(ar[i].Length <= 3)
+        {
+        ar2[c] = ar[i];
+        c++;
+        }
+    }
+}
